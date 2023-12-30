1dcacc192847ef7fbe3ef2a3d3787dbf3986dc44
tamasstephen <kicsitnagyon@gmail.com>
2023-12-30 14:09:23 +0100

    feat: add starter files

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className=" text-5xl font-semibold font-general">See products</h1>
+    </div>
+  );
+}
+
+export default App;
